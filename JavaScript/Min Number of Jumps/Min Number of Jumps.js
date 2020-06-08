a5a20d85316b7f829308721f8d28859bcae294e1
Dylan P <59612653+dnpolice@users.noreply.github.com>
2020-06-08 17:53:12 -0400

    Create Min Number of Jumps.js

---
+++ JavaScript/Min Number of Jumps/Min Number of Jumps.js
@@ -0,0 +1,16 @@
+function minNumberOfJumps(array) {
+	if(array.length === 1) return 0;
+	let steps = array[0];
+	//Farthest can go using what you have before steps reach zero
+	let maxReach = array[0];
+	let jumps = 0;
+	for(let i = 1; i < array.length-1; i++){
+		maxReach = Math.max(maxReach, i + array[i])
+		steps--;
+		if (steps === 0){
+			jumps++;
+			steps = maxReach - i;
+		}
+	}
+	return jumps+1;
+}
